3cb0ce949bc684201f72e500c942026b904903d9
Djellal Elkacem <djellal.elkacem@gmail.com>
2025-03-25 22:50:38 +0100

    Add project files.

---
+++ Course.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CeilApp.Models
+{
+    public class Course
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(30)]
+        public string Code { get; set; } = "";
+
+        [MaxLength(250)]
+        public string Name { get; set; } = "";
+
+        [MaxLength(250)]
+        public string NameAr { get; set; } = "";
+
+        public bool IsActive { get; set; } = true;
+        public int Duration { get; set; } = 0;
+
+        [ForeignKey("CourseType")]
+        public int CourseTypeId { get; set; }
+        public CourseType? CourseType { get; set; }
+    }
+}
